79a6de53997fcbbc8ee8e870639e46eff2adfbe1
GustavoHerrador <gustavoherrador10@gmail.com>
2022-06-15 15:04:25 -0300

    Styles Changes

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -4,20 +4,17 @@ import "././Item.css";
 
 function Item(item) {
   return (
-    <>
-      <Link to={`/item/${item.id}`}>
-        <h1 className="productsName">{item && item.name}</h1>
-        <img
-          src={item && item.image}
-          className="products"
-          alt="...
-      "
-        />
+    <div className="productcard">
+      <div>
+        <Link to={`/item/${item.id}`}>
+          <h1 className="productsName">{item && item.name}</h1>
+          <img src={item && item.image} className="products" alt="..." />
+        </Link>
         <h2>Id del producto: {item && item.id}</h2>
         <h3>Precio por unidad: {item && item.price}</h3>
         <h4>Acerca del producto :</h4> <p>{item && item.des}</p>
-      </Link>
-    </>
+      </div>
+    </div>
   );
 }
 
